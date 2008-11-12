fb0238f53435606e2c9f3249558c3b1d0b49bd59
Francois Keith <francois.keith@aist.go.jp>
2008-11-12 11:10:41 +0900

    Add constants.h, file that will contain missing constant definition (e.g. the definition of M_PI for win32)

--- include/jrlMathTools/angle.h
+++ include/jrlMathTools/angle.h
@@ -7,6 +7,7 @@
 
 #include <iostream>
 #include <math.h>
+#include "constants.h"
 
 namespace jrlMathTools {
 
---
+++ include/jrlMathTools/constants.h
@@ -0,0 +1,14 @@
+/*
+ *   Define usual constants
+ *  Copyright 2008 Francois Keith
+ */
+
+#ifndef JRLMATHTOOLS_CONSTANTS_H
+#define JRLMATHTOOLS_CONSTANTS_H
+
+#include <math.h>
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
+#endif
