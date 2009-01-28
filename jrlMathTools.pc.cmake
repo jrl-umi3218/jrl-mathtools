352fc51b67e2353feaa30284eef4a10d921fe284
florent <florent@big-laptop.(none)>
2009-01-28 13:52:47 +0900

    Removed obsolete autotools files. (forgot -a option)

--- jrlMathTools.pc.cmake
+++ jrlMathTools.pc.cmake
@@ -1,13 +1,13 @@
 prefix=${CMAKE_INSTALL_PREFIX}
-exec_prefix=${install_pkg_prefix}/bin
-libdir=${install_pkg_prefix}/lib
+exec_prefix=${install_pkg_prefix}
+libdir=${install_pkg_exec_prefix}/lib
 includedir=${install_pkg_prefix}/include
 datarootdir=${install_pkg_prefix}/share
-docdir=${install_pkg_prefix}/doc/
+docdir=${install_pkg_datarootdir}/doc/${PROJECT_NAME}
 
 Name: ${PROJECT_NAME}
 Description: 
 Version: ${PROJECT_VERSION}
 Requires: 
 Libs: 
-Cflags: -I${install_pkg_prefix}/include
+Cflags: -I${install_pkg_include_dir}
